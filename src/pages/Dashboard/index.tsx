3df496585f962c5a8270d8482e89c4a13fcc8503
erenildo <dev.erenildo@gmail.com>
2025-11-04 18:27:11 -0300

    remove index dashboard comentado

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,35 +1,3 @@
-// import { useDispatch, useSelector } from 'react-redux';
-// import type { AppDispatch, RootState } from '../../app/store';
-// import { logout } from '../../features/auth/redux/authSlice';
-// import { Navigate } from 'react-router-dom';
-
-// const Dashboard = () => {
-//     const dispatch = useDispatch<AppDispatch>();
-
-//     const { isAuthenticated } = useSelector((state: RootState) => state.auth);
-
-//     const handleLogout = () => {
-//         dispatch(logout());
-//     };
-
-//     if (!isAuthenticated) {
-//         return <Navigate to="/login" replace />;
-//     }
-
-//     return (
-//         <div>
-//             <h1>Página Logada (Dashboard)</h1>
-//             <p>Você está autenticado com sucesso.</p>
-//             <button onClick={handleLogout}>
-//                 Logout
-//             </button>
-//         </div>
-//     );
-// };
-
-// export default Dashboard;
-
-
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import type { AppDispatch, RootState } from '../../app/store';
