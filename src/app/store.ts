361b0e8f55a3c28c7bdfe331126bfe28e657c353
erenildo <dev.erenildo@gmail.com>
2025-11-04 17:58:48 -0300

    adiciona reducer cartao ao redux global

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/redux/authSlice";
+import cartaoReducer from "../features/cartao/redux/cartaoSlice";
 import { setupInterceptors } from "../services/setupInterceptors";
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer,
+        cartao: cartaoReducer
     }
 });
 
