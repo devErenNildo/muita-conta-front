0a5053b4032a1b9da60be3484da0e90aa3527764
erenildo <dev.erenildo@gmail.com>
2025-11-18 14:18:25 -0300

    adiciona props de erro no input Props

--- src/shared/components/atoms/GlassInput.tsx
+++ src/shared/components/atoms/GlassInput.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './GlassInput.module.css';
 
-type Props = React.InputHTMLAttributes<HTMLInputElement> & {};
+type Props = React.InputHTMLAttributes<HTMLInputElement> & {
+    hasError?: boolean;
+};
 
-const GlassInput = (props: Props) => {
+const GlassInput = ({ hasError, className, ...props }: Props) => {
     return (
         <input 
             {...props} 
-            className={`${styles.input} ${props.className || ''}`} 
+            className={`${styles.input} ${hasError ? styles.error : ''} ${className || ''}`}
         />
     );
 };
